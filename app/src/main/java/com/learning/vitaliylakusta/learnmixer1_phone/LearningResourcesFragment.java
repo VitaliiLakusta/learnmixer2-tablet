fa7293c8ef7ccf65a33f543a3096d78173219107
Vitalii Lakusta <lakusta96@gmail.com>
2015-03-17 02:18:56 +0200

    sidebar can be closed now. the main layout adjusts accordingly after fragment removal; all done

--- app/src/main/java/com/learning/vitaliylakusta/learnmixer1_phone/LearningResourcesFragment.java
+++ app/src/main/java/com/learning/vitaliylakusta/learnmixer1_phone/LearningResourcesFragment.java
@@ -3,15 +3,20 @@ package com.learning.vitaliylakusta.learnmixer1_phone;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -33,12 +38,26 @@ public class LearningResourcesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final ViewGroup finalContainer = container;
         View view = inflater.inflate(R.layout.fragment_item_list, container, false);
 
-        resourcesListView = (ListView) view.findViewById(R.id.resourceTitle);
+        resourcesListView = (ListView) view.findViewById(R.id.resourcesListView);
         String[] resourcesList = { "Learning resource 1", "Learning resource 2", "Learning resource 3" };
         resourceAdapter = new ResourceAdapter(this.getActivity(), resourcesList);
         resourcesListView.setAdapter(resourceAdapter);
+
+        Button btnCloseFragment = (Button) view.findViewById(R.id.btnCloseFragment);
+        btnCloseFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .remove(LearningResourcesFragment.this).commit();
+                RelativeLayout mainLayout = (RelativeLayout) getActivity().findViewById(R.id.mainLayout);
+                LinearLayout.LayoutParams mainLayoutParams = (LinearLayout.LayoutParams) mainLayout.getLayoutParams();
+                mainLayoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+                mainLayout.setLayoutParams(mainLayoutParams);
+            }
+        });
 
         return view;
     }
